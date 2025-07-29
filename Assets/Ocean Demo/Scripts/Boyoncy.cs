f4f4b61d5c5f9a2d1a1bd695cbfb14cf82f42475
parad <paradox_x94@hotmail.com>
2025-07-29 16:25:10 +0200

    add: LODs

---
+++ Assets/Ocean Demo/Scripts/Boyoncy.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Boyoncy : MonoBehaviour
+{
+    [SerializeField] OceanWaveController _WaveController;
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Ocean Demo/Scripts/OceanWaveController.cs
@@ -0,0 +1,67 @@
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class OceanWaveController : MonoBehaviour
+{
+    public Material[] oceanMaterials;
+    
+    [Range(0, 1)]
+    public float stormy = 360;
+
+    public Vector4[] waveDirections = new Vector4[64];
+    
+    public float windDirection = 60;
+
+    private void Start()
+    {
+        Initialize();
+    }
+
+    private void OnValidate()
+    {
+        Reinitialize();
+    }
+
+    private void Initialize()
+    {
+        float angle = Mathf.Lerp(360, 45, stormy) * Mathf.Deg2Rad;
+        float initDir = windDirection * Mathf.Deg2Rad; 
+        Vector4[] result = new Vector4[waveDirections.Length];
+        for (int i = 0; i < waveDirections.Length; i++)
+        {
+            float a = Random.Range(0f, Mathf.PI * angle) + initDir;
+            float x = Mathf.Cos(a);
+            float y = Mathf.Sin(a);
+            result[i] = new Vector4(x, y, 0f, 0f);
+            waveDirections[i] = new Vector4(x, y, 0f, 0f);
+        }
+
+        WriteToMaterials(stormy, result);
+    }
+
+    private void Reinitialize()
+    {
+        float angle = Mathf.Lerp(360, 45, stormy) * Mathf.Deg2Rad;
+        float initDir = windDirection * Mathf.Deg2Rad; 
+        Vector4[] result = new Vector4[waveDirections.Length];
+        for (int i = 0; i < waveDirections.Length; i++)
+        {
+            float a = Mathf.Acos(waveDirections[i].x) * angle + initDir;
+            float x = Mathf.Cos(a);
+            float y = Mathf.Sin(a);
+            result[i] = new Vector4(x, y, 0f, 0f);
+        }
+        
+        WriteToMaterials(stormy, result);
+    }
+
+    private void WriteToMaterials(float storm, Vector4[] waves)
+    {
+        foreach (var mat in oceanMaterials)
+        {
+            mat.SetVectorArray("_WaveDirs", waves);
+            mat.SetFloat("_WaveLength", Mathf.Lerp(5, 30, storm));
+            mat.SetFloat("_WaveStrength", Mathf.Lerp(.01f, 1.5f, storm));
+        }
+    }
+}
