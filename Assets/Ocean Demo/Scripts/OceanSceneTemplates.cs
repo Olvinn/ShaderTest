5bcce9ae966cdf57eec345bc7695195d41e158a0
parad <paradox_x94@hotmail.com>
2025-08-19 19:50:48 +0200

    add: working templates

---
+++ Assets/Ocean Demo/Scripts/OceanSceneTemplates.cs
@@ -0,0 +1,23 @@
+using System;
+using UnityEngine;
+
+namespace Ocean_Demo.Scripts
+{
+    [Serializable]
+    public struct Template
+    {
+        public string Name;
+        public float Storm;
+        public Cubemap Cubemap;
+        public Color FogColor;
+        public Vector3 DirLightDirection;
+        public float DirLightIntensity;
+        public Color DirLightColor;
+    }
+    
+    [CreateAssetMenu(menuName = "Ocean Scene Templates")]
+    public class OceanSceneTemplates : ScriptableObject
+    {
+        public Template[] templates;
+    }
+}
--- Assets/Ocean Demo/Scripts/SceneController.cs
+++ Assets/Ocean Demo/Scripts/SceneController.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Linq;
 using TMPro;
 using UnityEngine;
 
@@ -5,29 +7,30 @@ namespace Ocean_Demo.Scripts
 {
     public class SceneController : MonoBehaviour
     {
+        [SerializeField] private Light _directionLight;
+        [SerializeField] private OceanSceneTemplates _templates;
         [SerializeField] private OceanWaveController _oceanWaveController; 
         [SerializeField] private TMP_Dropdown _templateDropdown;
 
         private void Start()
         {
+            _templateDropdown.options = new List<TMP_Dropdown.OptionData>(_templates.templates.Select(v => new TMP_Dropdown.OptionData(v.Name)));
             _templateDropdown.onValueChanged.AddListener(ChangeTemplate);
             ChangeTemplate(_templateDropdown.value);
         }
 
         private void ChangeTemplate(int templateNum)
         {
-            switch (templateNum)
-            {
-                case 0:
-                    _oceanWaveController.ChangeWater(0);
-                    break;
-                case 1:
-                    _oceanWaveController.ChangeWater(.15f);
-                    break;
-                case 2:
-                    _oceanWaveController.ChangeWater(1f);
-                    break;
-            }
+            _oceanWaveController.ChangeWater(_templates.templates[templateNum].Storm);
+            var skyMat = RenderSettings.skybox;
+            skyMat.SetTexture("_Tex", _templates.templates[templateNum].Cubemap);
+            RenderSettings.fogColor = _templates.templates[templateNum].FogColor;
+            RenderSettings.defaultReflectionMode = UnityEngine.Rendering.DefaultReflectionMode.Custom;
+            RenderSettings.customReflectionTexture = _templates.templates[templateNum].Cubemap;
+            _directionLight.transform.rotation = Quaternion.Euler(_templates.templates[templateNum].DirLightDirection);
+            _directionLight.color = _templates.templates[templateNum].DirLightColor;
+            _directionLight.intensity = _templates.templates[templateNum].DirLightIntensity;
+            DynamicGI.UpdateEnvironment();
         }
     }
 }
